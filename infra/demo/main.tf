a9b1c5b74c4a9fc6ac9566de61a1e6894b0511fe
Shaoyun Jian <110411867+shaoyunjian@users.noreply.github.com>
2025-10-18 20:35:19 +0100

    chore: add Terraform demo and Atlantis config

---
+++ infra/demo/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6"
+    }
+  }
+}
+provider "random" {}
+
+resource "random_id" "example" {
+  byte_length = 4
+}
+
+output "hex" {
+  value = random_id.example.hex
+}
